1fb9dc89d6a2662807c2d1fbf6b52869477b3197
Loeka Lievens <loekalievens@hotmail.com>
2021-12-02 12:02:04 +0100

    docker

--- challenge-3/appservice.tf
+++ challenge-3/appservice.tf
@@ -21,12 +21,15 @@ resource "azurerm_app_service_plan" "appserviceplan" {
   name                = "${azurerm_resource_group.group.name}-plan"
   location            = azurerm_resource_group.group.location
   resource_group_name = azurerm_resource_group.group.name
-  reserved = true # Leave this as it is mandatory for Linux plans
+  reserved            = true # Leave this as it is mandatory for Linux plans
 
   # Define Linux as Host OS
-
+  kind = "Linux"
   # Choose Standard Tier, Size = S1
-  
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
 }
 
 ## Azure App Service enables you to build and host web apps, mobile back ends, and RESTful APIs in the programming language of your choice without managing infrastructure. 
@@ -39,11 +42,11 @@ resource "azurerm_app_service" "app" {
   resource_group_name = azurerm_resource_group.group.name
 
   ## Uncomment The Line below !!!!! Link me with the plan by using the id of the item created above
-  ## app_service_plan_id = azurerm_app_service_plan.app...
+  app_service_plan_id = azurerm_app_service_plan.appserviceplan.id
 
   # Configure Docker Image mvandep3/htf:latest here
   site_config {
-    linux_fx_version = ""
+    linux_fx_version = "mvandep3/htf:latest"
   }
 
   # Configure your team ID above
